e866aa2f2cc58cdd29b55a318e7b800c0291247f
vith13 <evsa@ic.ufal.br>
2026-03-29 15:30:06 -0300

    finaliza tela de login e cria a rota pra tela inicial

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def login():
     return render_template("login.html")
 
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
+

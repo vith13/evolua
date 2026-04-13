7fbff7f8652f16d41c9c2f843a1f4e57ee04349a
vith13 <evsa@ic.ufal.br>
2026-04-13 19:05:44 -0300

    implementa telas até a tela de sessões e implementa também template herança

--- app.py
+++ app.py
@@ -8,7 +8,20 @@ def login():
 
 @app.route("/dashboard")
 def dashboard():
-    return render_template("dashboard.html")
+    return render_template("dashboard.html", pagina_ativa="dashboard")
+
+@app.route("/tarefas")
+def tarefas():
+    return render_template("tarefas.html", pagina_ativa="tarefas")
+
+@app.route("/disciplinas")
+def disciplinas():
+    return render_template("disciplinas.html", pagina_ativa="disciplinas")
+
+@app.route("/sessoes")
+def sessoes():
+    return render_template("sessoes.html", pagina_ativa="sessoes")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
